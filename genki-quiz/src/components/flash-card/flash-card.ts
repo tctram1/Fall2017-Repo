a0c36f8ab857680a1d652df9d6acec84afa4c85e
tctram1 <tctram1@catamount.wcu.edu>
2017-10-29 11:29:58 -0400

    Commit

---
+++ genki-quiz/src/components/flash-card/flash-card.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'flash-card',
+  templateUrl: 'flash-card.html'
+})
+
+export class FlashCardComponent {
+
+  @Input('isFlipped') flipCard: boolean;
+
+  constructor() {}
+
+
+
+
+
+
+} //end of export class
--- kid-quiz/src/pages/home/home.ts
+++ kid-quiz/src/pages/home/home.ts
@@ -1,14 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Data } from '../../providers/data/data';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('slides') slides: any;
 
-  }
+  hasAnswered: boolean = false;
+  score: number = 0;
 
-}
+  slideOptions: any;
+  questions: any;
+
+  constructor(public navCtrl: NavController,
+              public dataService: Data) {}
+
+  ionViewDidLoad() {
+
+    this.slides.lockSwipes(true);
+
+    this.dataService.load().then((data) => {
+
+      data.map((question) => {
+
+        let originalOrder = question.answers;
+        question.answers = this.randomizeAnswers(originalOrder);
+        return question;
+
+      });
+
+      this.questions = data
+
+    });
+
+  } //end of ionViewDidLoad()
+
+  nextSlide() {
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  } //end of nextSlide()
+
+  selectAnswer(answer, question) {
+    
+    this.hasAnswered = true;
+    answer.selected = true;
+    question.flashCardFlipped = true;
+
+    if(answer.correct) {
+      this.score++;
+    }
+
+    setTimeout(() => {
+      this.hasAnswered = false;
+      this.nextSlide();
+      answer.selected = false;
+      question.flashCardFlipped = false;
+    }, 3000);
+
+  } //end of selectAnswer()
+
+  randomizeAnswers(rawAnswers: any[]): any[] {
+
+    for (let i = rawAnswers.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = rawAnswers[i];
+      rawAnswers[i] = rawAnswers[j];
+      rawAnswers[j] = temp;
+    }
+
+    return rawAnswers;
+
+  } //end of randomizeAnswers()
+
+  restartQuiz() {
+    this.score = 0;
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(1, 1000);
+    this.slides.lockSwipes(true);
+  } //end of restartQuiz()
+
+
+
+
+} //end of export class
