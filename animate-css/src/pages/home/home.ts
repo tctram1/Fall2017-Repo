89f2c2453aa787aec0741708a042b47c68ef85ed
tctram1 <tctram1@catamount.wcu.edu>
2017-12-03 16:18:23 -0500

    Finish Testing

--- animate-css/src/pages/home/home.ts
+++ animate-css/src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  refreshPage() {
+    this.navCtrl.setRoot(this.navCtrl.getActive().component);
   }
 
-}
+
+
+} //end of export class
